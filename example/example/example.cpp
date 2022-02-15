f2ef6b4ebb28589fca6fd45b0b72e612604c8232
ArseniyKichkin <kichkin.arsenii@phystech.edu>
2022-02-15 18:40:38 +0300

    Homework folders added

---
+++ example/example/example.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+template<typename T>
+
+class AutoPtr {
+public:
+	AutoPtr(T* data) : m_data(data) {}
+	~AutoPtr() {
+		if (m_data != nullptr)
+			delete m_data;
+	}
+private:
+	T* m_data;
+};
+
+class Student {
+public:
+	Student() { std::cout << "I became a student\n"; }
+	~Student() { std::cout << "I did not pass exams\n"; }
+};
+
+
+int main() {
+	AutoPtr<Student> ptr_student(new Student());
+	{
+		AutoPtr<Student> ptr_student2(new Student());
+	}
+	std::cout << "Finish";
+}
